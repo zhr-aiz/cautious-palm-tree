3be9eb05ffbc471f5501b9719d9cc4690b3ddf72
zhr-aiz <3360332957@qq.com>
2026-06-02 15:19:36 +0800

    feat: add vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/cautious-palm-tree/',
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
+});
